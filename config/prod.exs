1ef25b61fc0a5837728c9bde1e51c5a38542efd5
Junan Chakma <junan@nimblehq.co>
2021-05-25 16:00:24 +0700

    Use comma

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :elixir_google_scraper, ElixirGoogleScraperWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("HOST"), port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
