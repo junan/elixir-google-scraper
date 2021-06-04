98cfe5401275275017af67d4b704dc3f4a7d6023
Junan Chakma <junan@nimblehq.co>
2021-06-04 15:41:06 +0700

    Change text

--- test/elixir_google_scraper_web/features/password_reset/view_password_reset_page_test.exs
+++ test/elixir_google_scraper_web/features/password_reset/view_password_reset_page_test.exs
@@ -3,7 +3,7 @@ defmodule ElixirGoogleScraperWeb.PasswordReset.ViewPasswordResetPageTest do
 
   @path Routes.user_settings_path(ElixirGoogleScraperWeb.Endpoint, :edit)
 
-  feature "views new upload page", %{session: session} do
+  feature "views password reset page", %{session: session} do
     session
     |> login_with_user
     |> visit(@path)
