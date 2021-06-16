4bc3a3521144833f8ad6fb081459a34524ae3995
Junan Chakma <junan@nimblehq.co>
2021-06-16 11:09:32 +0700

    Add controller test

--- test/elixir_google_scraper/scraper_test.exs
+++ test/elixir_google_scraper/scraper_test.exs
@@ -2,13 +2,10 @@ defmodule ElixirGoogleScraper.ScraperTest do
   use ElixirGoogleScraper.DataCase, async: true
 
   alias ElixirGoogleScraper.Scraper
+  alias ElixirGoogleScraper.Scraper.Keyword
 
-  describe "keywords" do
-    alias ElixirGoogleScraper.Scraper.Keyword
-
-    @invalid_attrs %{name: nil, status: nil, user_id: nil}
-
-    test "create_keyword/1 with valid data creates a keyword" do
+  describe "create_keyword/1" do
+    test "creates a keyword with valid data" do
       user = insert(:user)
       valid_attrs = %{name: "some name", status: :pending, user_id: user.id}
 
@@ -18,8 +15,27 @@ defmodule ElixirGoogleScraper.ScraperTest do
       assert keyword.user_id == user.id
     end
 
-    test "create_keyword/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Scraper.create_keyword(@invalid_attrs)
+    test "does not create keyword with invalid data" do
+      invalid_attrs = %{name: nil, status: nil, user_id: nil}
+      assert {:error, %Ecto.Changeset{}} = Scraper.create_keyword(invalid_attrs)
+    end
+  end
+
+  describe "save_keywords/2" do
+    test "stores the keywords" do
+      user = insert(:user)
+      file = %Plug.Upload{content_type: "text/csv", path: "test/fixture/keywords.csv"}
+
+      assert {:ok, :file_is_proccessed} = Scraper.save_keywords(file, user)
+
+      assert [keyword1, keyword2] = Repo.all(Keyword)
+
+      assert keyword1.name == "buy domain"
+      assert keyword1.user_id == user.id
+      assert keyword1.status == :pending
+      assert keyword2.name == "buy bike"
+      assert keyword2.user_id == user.id
+      assert keyword2.status == :pending
     end
   end
 end
---
+++ test/elixir_google_scraper_web/controllers/keyword_controller_test.exs
@@ -0,0 +1,50 @@
+defmodule ElixirGoogleScraperWeb.KeywordControllerTest do
+  use ElixirGoogleScraperWeb.ConnCase, async: true
+
+  describe "GET index/2" do
+    test "returns 200 status code when accessing with a logged-in user", %{conn: conn} do
+      user = insert(:user)
+
+      conn =
+        conn
+        |> log_in_user(user)
+        |> get(Routes.keyword_path(conn, :index))
+
+      assert html_response(conn, 200) =~ "Upload"
+    end
+  end
+
+  describe "POST create/2" do
+    test "redirects to the keywords index page when the CVS file uploaded successfully", %{
+      conn: conn
+    } do
+      file = %Plug.Upload{content_type: "text/csv", path: "test/fixture/keywords.csv"}
+      user = insert(:user)
+      params = %{:file => file}
+
+      conn =
+        conn
+        |> log_in_user(user)
+        |> post(Routes.keyword_path(conn, :create), params)
+
+      assert redirected_to(conn) == Routes.keyword_path(conn, :index)
+      assert get_flash(conn, :info) == "Your CSV file has been uploaded successfully"
+    end
+  end
+
+  test "render error message when CVS file is empty", %{
+    conn: conn
+  } do
+    file = %Plug.Upload{content_type: "text/csv", path: "test/fixture/empty_keywords.csv"}
+    user = insert(:user)
+    params = %{:file => file}
+
+    conn =
+      conn
+      |> log_in_user(user)
+      |> post(Routes.keyword_path(conn, :create), params)
+
+    assert redirected_to(conn) == Routes.keyword_path(conn, :index)
+    assert get_flash(conn, :error) == "File can't be blank"
+  end
+end
