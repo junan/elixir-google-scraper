84c5ebb12ec2a4a38c529249e49793ad0a0019e1
Junan Chakma <junan@nimblehq.co>
2021-06-16 11:40:52 +0700

    Add test

--- test/elixir_google_scraper_web/controllers/page_controller_test.exs
+++ test/elixir_google_scraper_web/controllers/page_controller_test.exs
@@ -1,3 +1,8 @@
 defmodule ElixirGoogleScraperWeb.PageControllerTest do
   use ElixirGoogleScraperWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Welcome to Google Scaraper App"
+  end
 end
