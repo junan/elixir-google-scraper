e9d35702e1475917ffb4f28c6c705fea5b140678
Junan Chakma <junan@nimblehq.co>
2021-06-16 09:50:59 +0700

    Update test

--- lib/elixir_google_scraper/scraper.ex
+++ lib/elixir_google_scraper/scraper.ex
@@ -11,35 +11,6 @@ defmodule ElixirGoogleScraper.Scraper do
   alias ElixirGoogleScraper.Scraper.Keyword
 
   @doc """
-  Returns the list of keywords.
-
-  ## Examples
-
-      iex> list_keywords()
-      [%Keyword{}, ...]
-
-  """
-  def list_keywords do
-    Repo.all(Keyword)
-  end
-
-  @doc """
-  Gets a single keyword.
-
-  Raises `Ecto.NoResultsError` if the Keyword does not exist.
-
-  ## Examples
-
-      iex> get_keyword!(123)
-      %Keyword{}
-
-      iex> get_keyword!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_keyword!(id), do: Repo.get!(Keyword, id)
-
-  @doc """
   Creates a keyword.
 
   ## Examples
@@ -55,53 +26,6 @@ defmodule ElixirGoogleScraper.Scraper do
     %Keyword{}
     |> Keyword.changeset(attrs)
     |> Repo.insert()
-  end
-
-  @doc """
-  Updates a keyword.
-
-  ## Examples
-
-      iex> update_keyword(keyword, %{field: new_value})
-      {:ok, %Keyword{}}
-
-      iex> update_keyword(keyword, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_keyword(%Keyword{} = keyword, attrs) do
-    keyword
-    |> Keyword.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a keyword.
-
-  ## Examples
-
-      iex> delete_keyword(keyword)
-      {:ok, %Keyword{}}
-
-      iex> delete_keyword(keyword)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_keyword(%Keyword{} = keyword) do
-    Repo.delete(keyword)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking keyword changes.
-
-  ## Examples
-
-      iex> change_keyword(keyword)
-      %Ecto.Changeset{data: %Keyword{}}
-
-  """
-  def change_keyword(%Keyword{} = keyword, attrs \\ %{}) do
-    Keyword.changeset(keyword, attrs)
   end
 
   def save_keywords(file, %User{} = user) do
--- test/elixir_google_scraper/scraper_test.exs
+++ test/elixir_google_scraper/scraper_test.exs
@@ -1,72 +1,25 @@
 defmodule ElixirGoogleScraper.ScraperTest do
-  use ElixirGoogleScraper.DataCase
+  use ElixirGoogleScraper.DataCase, async: true
 
   alias ElixirGoogleScraper.Scraper
 
   describe "keywords" do
     alias ElixirGoogleScraper.Scraper.Keyword
 
-    @valid_attrs %{name: "some name", status: "some status", user_id: "some user_id"}
-    @update_attrs %{
-      name: "some updated name",
-      status: "some updated status",
-      user_id: "some updated user_id"
-    }
     @invalid_attrs %{name: nil, status: nil, user_id: nil}
 
-    def keyword_fixture(attrs \\ %{}) do
-      {:ok, keyword} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Scraper.create_keyword()
-
-      keyword
-    end
-
-    test "list_keywords/0 returns all keywords" do
-      keyword = keyword_fixture()
-      assert Scraper.list_keywords() == [keyword]
-    end
-
-    test "get_keyword!/1 returns the keyword with given id" do
-      keyword = keyword_fixture()
-      assert Scraper.get_keyword!(keyword.id) == keyword
-    end
-
     test "create_keyword/1 with valid data creates a keyword" do
-      assert {:ok, %Keyword{} = keyword} = Scraper.create_keyword(@valid_attrs)
+      user = insert(:user)
+      valid_attrs = %{name: "some name", status: :pending, user_id: user.id}
+
+      assert {:ok, %Keyword{} = keyword} = Scraper.create_keyword(valid_attrs)
       assert keyword.name == "some name"
-      assert keyword.status == "some status"
-      assert keyword.user_id == "some user_id"
+      assert keyword.status == :pending
+      assert keyword.user_id == user.id
     end
 
     test "create_keyword/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Scraper.create_keyword(@invalid_attrs)
-    end
-
-    test "update_keyword/2 with valid data updates the keyword" do
-      keyword = keyword_fixture()
-      assert {:ok, %Keyword{} = keyword} = Scraper.update_keyword(keyword, @update_attrs)
-      assert keyword.name == "some updated name"
-      assert keyword.status == "some updated status"
-      assert keyword.user_id == "some updated user_id"
-    end
-
-    test "update_keyword/2 with invalid data returns error changeset" do
-      keyword = keyword_fixture()
-      assert {:error, %Ecto.Changeset{}} = Scraper.update_keyword(keyword, @invalid_attrs)
-      assert keyword == Scraper.get_keyword!(keyword.id)
-    end
-
-    test "delete_keyword/1 deletes the keyword" do
-      keyword = keyword_fixture()
-      assert {:ok, %Keyword{}} = Scraper.delete_keyword(keyword)
-      assert_raise Ecto.NoResultsError, fn -> Scraper.get_keyword!(keyword.id) end
-    end
-
-    test "change_keyword/1 returns a keyword changeset" do
-      keyword = keyword_fixture()
-      assert %Ecto.Changeset{} = Scraper.change_keyword(keyword)
     end
   end
 end
