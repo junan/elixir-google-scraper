071bbe86f57e8865844f94d0be88389cefa634c1
Junan Chakma <junan@nimblehq.co>
2021-06-17 13:33:05 +0700

    Add validate method test

---
+++ test/elixir_google_scraper/scraper/csv_keyword_test.exs
@@ -0,0 +1,21 @@
+defmodule ElixirGoogleScraper.Scraper.CSVKeywordTest do
+  use ElixirGoogleScraper.DataCase
+
+  alias ElixirGoogleScraper.Scraper.CSVKeyword
+
+  describe "validate/2" do
+    test "returns ok when keyword file is valid" do
+      file = %Plug.Upload{content_type: "text/csv", path: "test/fixture/keywords.csv"}
+
+      assert {:ok, _} = CSVKeyword.validate(file)
+    end
+
+    test "returns an error when keyword file is INVALID" do
+      file = %Plug.Upload{content_type: "text/csv", path: "test/fixture/empty_keywords.csv"}
+
+      IO.inspect(file, label: "File")
+
+      assert {:error, :file_is_empty} = CSVKeyword.validate(file)
+    end
+  end
+end
