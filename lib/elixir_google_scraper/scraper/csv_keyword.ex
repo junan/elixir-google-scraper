74dc5a0d63fb1c118c6eff0d90431f751e605939
Junan Chakma <junan@nimblehq.co>
2021-06-17 11:32:19 +0700

    use case instead of cond

--- lib/elixir_google_scraper/scraper/csv_keyword.ex
+++ lib/elixir_google_scraper/scraper/csv_keyword.ex
@@ -4,14 +4,14 @@ defmodule ElixirGoogleScraper.Scraper.CSVKeyword do
   def validate(file) do
     keyword_list = parse(file)
 
-    cond do
-      length(keyword_list) <= 0 -> {:error, :file_is_empty}
-      length(keyword_list) > 1000 -> {:error, :keyword_list_exceed}
-      true -> {:ok, keyword_list}
+    case length(keyword_list) do
+      length when length <= 0 -> {:error, :file_is_empty}
+      length when length > 1000 -> {:error, :keyword_list_exceeded}
+      _ -> {:ok, keyword_list}
     end
   end
 
-  def parse(%Plug.Upload{content_type: "text/csv"} = file) do
+  defp parse(%Plug.Upload{content_type: "text/csv"} = file) do
     file.path
     |> File.stream!()
     |> CSV.parse_stream(skip_headers: false)
