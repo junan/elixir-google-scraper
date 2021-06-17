6e321250b0973334e06498d1609379465e5595e7
Junan Chakma <junan@nimblehq.co>
2021-06-17 13:40:52 +0700

    Remove  IO.inspect

--- test/elixir_google_scraper/scraper/csv_keyword_test.exs
+++ test/elixir_google_scraper/scraper/csv_keyword_test.exs
@@ -13,8 +13,6 @@ defmodule ElixirGoogleScraper.Scraper.CSVKeywordTest do
     test "returns an error when keyword file is INVALID" do
       file = %Plug.Upload{content_type: "text/csv", path: "test/fixture/empty_keywords.csv"}
 
-      IO.inspect(file, label: "File")
-
       assert {:error, :file_is_empty} = CSVKeyword.validate(file)
     end
   end
