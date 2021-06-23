35f68acd97654d1803de4e12647fb477e453a01c
Junan Chakma <junan@nimblehq.co>
2021-06-23 19:35:59 +0700

    Add parsing service

--- lib/elixir_google_scraper/scraper/keyword_scraper.ex
+++ lib/elixir_google_scraper/scraper/keyword_scraper.ex
@@ -1,4 +1,26 @@
 defmodule ElixirGoogleScraper.Scraper.KeywordScraper do
   def scraping(keyword) do
+    make_request(keyword)
+  end
+
+  def google_search_url, do: "https://www.google.com/search"
+
+  defp make_request(keyword) do
+    keyword
+    |> build_url
+    |> HTTPoison.get!()
+    |> parse_response
+  end
+
+  defp build_url(keyword) do
+    "#{google_search_url}?q=#{keyword}"
+  end
+
+  def parse_response(response) do
+    {:ok, document} = Floki.parse_document(response.body)
+
+    IO.inspect(document, label: "Document")
+
+    document
   end
 end
