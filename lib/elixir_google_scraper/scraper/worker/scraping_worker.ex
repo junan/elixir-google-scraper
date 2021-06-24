c9f0ce44de59adb70a621e118ebca652cb8cb36d
Junan Chakma <junan@nimblehq.co>
2021-06-24 22:24:39 +0700

    Update worker

--- lib/elixir_google_scraper/scraper/worker/scraping_worker.ex
+++ lib/elixir_google_scraper/scraper/worker/scraping_worker.ex
@@ -1,17 +1,26 @@
 defmodule ElixirGoogleScraper.Scraper.Worker.ScrapingWorker do
   use Oban.Worker,
     priority: 1,
-    max_attempts: 3,
+    max_attempts: 10,
     unique: [period: 30]
 
   alias ElixirGoogleScraper.Repo
-  alias ElixirGoogleScraper.Scraper.{KeywordScraper, Keyword}
+  alias ElixirGoogleScraper.Scraper.{KeywordScraper, Keyword, SearchResult}
 
   @impl Worker
   def perform(%Oban.Job{args: %{"keyword_id" => keyword_id}}) do
     keyword = Repo.get_by!(Keyword, %{id: keyword_id})
 
-    KeywordScraper.scrap(keyword.name)
+    # Scaraping google search result and storing the parsed data
+    keyword.name
+    |> KeywordScraper.scrap()
+    |> SearchResult.changeset(%{keyword_id: keyword.id})
+    |> Repo.insert!()
+
+    # Updating keyword status as completed
+    keyword
+    |> Keyword.changeset(%{status: :completed})
+    |> Repo.update!()
 
     :ok
   end
