157527a5b43a96d206868ae3f9fb7176eb6e75bd
Junan Chakma <junan@nimblehq.co>
2021-06-25 11:31:43 +0700

    Re record the VCR

--- test/elixir_google_scraper/scraper/worker/scraping_worker_test.exs
+++ test/elixir_google_scraper/scraper/worker/scraping_worker_test.exs
@@ -13,11 +13,11 @@ defmodule ElixirGoogleScraper.Scraper.Worker.ScrapingWorkerTest do
 
         search_result = Repo.preload(keyword, :search_result).search_result
 
-        assert search_result.top_ads_count == 1
-        assert search_result.top_ads_urls == ["https://www.bsrbikeshop.com/"]
-        assert search_result.total_ads_count == 1
+        assert search_result.top_ads_count == 0
+        assert search_result.top_ads_urls == []
+        assert search_result.total_ads_count == 0
         assert Enum.count(search_result.result_urls) == 10
-        assert search_result.total_links_count == 152
+        assert search_result.total_links_count == 222
         assert search_result.keyword_id == keyword.id
         assert String.contains?(search_result.html, "<!doctype html>")
       end
