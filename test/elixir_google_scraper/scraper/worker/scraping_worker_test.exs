74e6a6a0511868f4b28af8f6d46ee8f88c2223b1
Junan Chakma <junan@nimblehq.co>
2021-06-25 10:44:59 +0700

    Add worker test

--- test/elixir_google_scraper/scraper/worker/scraping_worker_test.exs
+++ test/elixir_google_scraper/scraper/worker/scraping_worker_test.exs
@@ -1,8 +1,7 @@
 defmodule ElixirGoogleScraper.Scraper.Worker.ScrapingWorkerTest do
-  use ElixirGoogleScraper.DataCase
+  use ElixirGoogleScraper.DataCase, async: true
 
   alias ElixirGoogleScraper.Scraper.Worker.ScrapingWorker
-  # alias ElixirGoogleScraper.Scraper.SearchResult
 
   describe "perform/1" do
     test "creates the search result" do
@@ -12,12 +11,28 @@ defmodule ElixirGoogleScraper.Scraper.Worker.ScrapingWorkerTest do
 
         ScrapingWorker.perform(%Oban.Job{args: %{"keyword_id" => keyword.id}})
 
-        # search_result = Repo.reload(keyword).result
         search_result = Repo.preload(keyword, :search_result).search_result
 
-        # assert result.all_ads == []
-        # assert result.top_ads == []
-        assert search_result.result_count == 10
+        assert search_result.top_ads_count == 1
+        assert search_result.top_ads_urls == ["https://www.bsrbikeshop.com/"]
+        assert search_result.total_ads_count == 1
+        assert Enum.count(search_result.result_urls) == 10
+        assert search_result.total_links_count == 152
+        assert search_result.keyword_id == keyword.id
+        assert String.contains?(search_result.html, "<!doctype html>")
+      end
+    end
+
+    test "updates the keyword as completed" do
+      use_cassette "scraper/keyword_buy_bike" do
+        user = insert(:user)
+        keyword = insert(:keyword, name: "buy bike", user: user)
+
+        ScrapingWorker.perform(%Oban.Job{args: %{"keyword_id" => keyword.id}})
+
+        keyword = Repo.reload(keyword)
+
+        assert keyword.status == :completed
       end
     end
   end
