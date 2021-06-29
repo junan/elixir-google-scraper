84315e6a5b868792e9683306c0143c7baabec3fd
Junan Chakma <junan@nimblehq.co>
2021-06-29 09:37:38 +0700

    Format code

--- test/elixir_google_scraper/scraper/schemas/search_response_test.exs
+++ test/elixir_google_scraper/scraper/schemas/search_response_test.exs
@@ -7,7 +7,7 @@ defmodule ElixirGoogleScraper.Scraper.Schemas.SearchResultTest do
     @valid_attrs %{total_links_count: 200, result_count: 10, html: "<html></html>"}
 
     test "requires the result_count" do
-      attr =  Map.put(@valid_attrs, :keyword_id, insert(:keyword).id)
+      attr = Map.put(@valid_attrs, :keyword_id, insert(:keyword).id)
 
       changeset = SearchResult.changeset(%SearchResult{}, Map.delete(attr, :result_count))
 
@@ -15,22 +15,20 @@ defmodule ElixirGoogleScraper.Scraper.Schemas.SearchResultTest do
     end
 
     test "requires the total_links_count" do
-      attr =  Map.put(@valid_attrs, :keyword_id, insert(:keyword).id)
+      attr = Map.put(@valid_attrs, :keyword_id, insert(:keyword).id)
 
       changeset = SearchResult.changeset(%SearchResult{}, Map.delete(attr, :total_links_count))
 
       refute changeset.valid?
     end
 
-
     test "requires the html" do
-      attr =  Map.put(@valid_attrs, :keyword_id, insert(:keyword).id)
+      attr = Map.put(@valid_attrs, :keyword_id, insert(:keyword).id)
 
       changeset = SearchResult.changeset(%SearchResult{}, Map.delete(attr, :html))
 
       refute changeset.valid?
     end
-
 
     test "requires the keyword_id" do
       changeset = SearchResult.changeset(%SearchResult{}, @valid_attrs)
