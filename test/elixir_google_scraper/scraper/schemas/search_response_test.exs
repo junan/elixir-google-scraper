88f8f11933ea3524fb9b506b7bbbee462b5c4f8b
Junan Chakma <junan@nimblehq.co>
2021-06-29 09:34:33 +0700

    Add search response spec

---
+++ test/elixir_google_scraper/scraper/schemas/search_response_test.exs
@@ -0,0 +1,41 @@
+defmodule ElixirGoogleScraper.Scraper.Schemas.SearchResultTest do
+  use ElixirGoogleScraper.DataCase, async: true
+
+  alias ElixirGoogleScraper.Scraper.Schemas.SearchResult
+
+  describe "changeset/2" do
+    @valid_attrs %{total_links_count: 200, result_count: 10, html: "<html></html>"}
+
+    test "requires the result_count" do
+      attr =  Map.put(@valid_attrs, :keyword_id, insert(:keyword).id)
+
+      changeset = SearchResult.changeset(%SearchResult{}, Map.delete(attr, :result_count))
+
+      refute changeset.valid?
+    end
+
+    test "requires the total_links_count" do
+      attr =  Map.put(@valid_attrs, :keyword_id, insert(:keyword).id)
+
+      changeset = SearchResult.changeset(%SearchResult{}, Map.delete(attr, :total_links_count))
+
+      refute changeset.valid?
+    end
+
+
+    test "requires the html" do
+      attr =  Map.put(@valid_attrs, :keyword_id, insert(:keyword).id)
+
+      changeset = SearchResult.changeset(%SearchResult{}, Map.delete(attr, :html))
+
+      refute changeset.valid?
+    end
+
+
+    test "requires the keyword_id" do
+      changeset = SearchResult.changeset(%SearchResult{}, @valid_attrs)
+
+      refute changeset.valid?
+    end
+  end
+end
