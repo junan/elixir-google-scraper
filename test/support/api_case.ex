63aeab0f4002e7fef782bd3e0f4aa9986681fcf1
Junan Chakma <junan@nimblehq.co>
2021-07-08 13:34:38 +0700

    Remove unused code

--- test/support/api_case.ex
+++ test/support/api_case.ex
@@ -8,19 +8,18 @@ defmodule ElixirGoogleScraperWeb.ApiCase do
 
       def authenticated_request(user, conn, target, type, params \\ %{}) do
         attrs = %{name: "Application", redirect_uri: "https://example.org/endpoint"}
-        file = %Plug.Upload{content_type: "text/csv", path: "test/fixture/keywords.csv"}
 
         {_, oauth_app} =
           Applications.create_application(nil, attrs, otp_app: :elixir_google_scraper)
 
-        {_, acess_token} =
+        {_, access_token} =
           AccessTokens.create_token(
             user,
             %{application: oauth_app},
             otp_app: :elixir_google_scraper
           )
 
-        conn = put_req_header(conn, "authorization", "Bearer " <> acess_token.token)
+        conn = put_req_header(conn, "authorization", "Bearer " <> access_token.token)
 
         if type == :post do
           post(conn, target, params)
