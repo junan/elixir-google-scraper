69451538b61db35190d5fc1c83a7868904231b65
Junan Chakma <junan@nimblehq.co>
2021-07-08 12:15:16 +0700

    Make DRY

--- lib/elixir_google_scraper_web/controllers/api/v1/token_controller.ex
+++ lib/elixir_google_scraper_web/controllers/api/v1/token_controller.ex
@@ -1,14 +1,13 @@
 defmodule ElixirGoogleScraperWeb.Api.V1.TokenController do
   use ElixirGoogleScraperWeb, :controller
 
-  alias ElixirGoogleScraperWeb.V1.ErrorView
-  alias ElixirGoogleScraperWeb.V1.TokenView
+  alias ElixirGoogleScraperWeb.V1.{ErrorView, TokenView}
 
   def create(conn, params) do
     case ExOauth2Provider.Token.grant(params, otp_app: :elixir_google_scraper) do
       {:ok, access_token} ->
         render(conn, TokenView, "show.json", %{
-          data: Map.put(access_token, :id, :rand.uniform(100))
+          data: Map.put(access_token, :id, :os.system_time(:seconds))
         })
 
       {:error, _, _} ->
